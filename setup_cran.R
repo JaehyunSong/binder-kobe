ff56bdde7c5694e0c83ef52b1c1dde55ecb5a400
Jaehyun Song <tintstyle@gmail.com>
2022-08-04 10:34:56 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -1,9 +1,18 @@
-install.packages("daggity",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("ggdag",     dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("rdd",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("rdrobust",  dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("rddensity", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("titanic",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("ghibli",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("pacman",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("gsynth",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+# Causal Inference
+install.packages("daggity",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("ggdag",        dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("rdd",          dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("rdrobust",     dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("rddensity",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("CausalImpact", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("gsynth",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+
+# Datasets
+install.packages("titanic",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+
+# Visualization
+install.packages("ghibli",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+
+# Package Manager
+install.packages("pacman",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+
