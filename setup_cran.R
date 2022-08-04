3a25727b5aeb52cec3cbb38133830d6c24dde5a4
Jaehyun Song <tintstyle@gmail.com>
2022-08-04 10:23:33 +0900

    Add files via upload

---
+++ setup_cran.R
@@ -0,0 +1,8 @@
+install.packages("daggity",  dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("ggdag",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("rdd",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("rdrobust", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("titanic",  dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("ghibli",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("pacman",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
+install.packages("gsynth",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
