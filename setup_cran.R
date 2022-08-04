0981a8e1b6842d015c35d7f0e4202312afc0e486
Jaehyun Song <tintstyle@gmail.com>
2022-08-04 10:57:41 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -7,12 +7,6 @@ install.packages("rddensity",    dependencies = TRUE, repos = "https://cran.ism.
 install.packages("CausalImpact", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("gsynth",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 
-# Datasets
-install.packages("titanic",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-
-# Visualization
-install.packages("ghibli",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-
 # Package Manager
 install.packages("pacman",       dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 
